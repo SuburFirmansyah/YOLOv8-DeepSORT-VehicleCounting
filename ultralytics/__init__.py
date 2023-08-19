3a6af3183b590209cacc002401d6c26f91680696
sfirmansyah <142659940+SuburFirmansyah@users.noreply.github.com>
2023-08-19 16:26:51 +0700

    ‎

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,13 +1,9 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
+# Ultralytics YOLO 🚀, GPL-3.0 license
 
-__version__ = '8.0.157'
+__version__ = "8.0.3"
 
-from ultralytics.hub import start
-from ultralytics.models import RTDETR, SAM, YOLO
-from ultralytics.models.fastsam import FastSAM
-from ultralytics.models.nas import NAS
-from ultralytics.utils import SETTINGS as settings
-from ultralytics.utils.checks import check_yolo as checks
-from ultralytics.utils.downloads import download
+from ultralytics.hub import checks
+from ultralytics.yolo.engine.model import YOLO
+from ultralytics.yolo.utils import ops
 
-__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'start', 'settings'  # allow simpler import
+__all__ = ["__version__", "YOLO", "hub", "checks"]  # allow simpler import
